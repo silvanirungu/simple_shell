a0b9e16e583ab8c6bac867691311e73f9e71c256
Moses Mumo <mosesm5499@gmail.com>
2023-09-20 04:25:17 +0300

    Update memory.c

--- memory.c
+++ memory.c
@@ -5,21 +5,21 @@
  *
  * @ptr: Pointer to the memory previously allocated
  * @old_size: Size, in bytes, of the allocated space for ptr
- * @new_size: New size, in bytes of the new memory block
- *
+ * @new_size: New size, in bytes of the new memory block                                        
+ *                                              
  * Return: Memory reallocated
  **/
-void *_realloc(void *ptr, size_t old_size, size_t new_size)
+void *_realloc(void *ptr, size_t old_size, size_t new_size)                                            
 {
 	char *s, *aux;
-	unsigned int a;
+	unsigned int a;                                        
 
-	if (new_size == old_size)
+	if (new_size == old_size)                                      
 		return (ptr);
 
 	if (ptr == NULL)
 	{
-		s = malloc(new_size);
+		s = malloc(new_size);                                                        
 		if (s == NULL)
 			return (NULL);
 
