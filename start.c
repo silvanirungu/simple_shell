351f7980edf3682fe1e0eb2cb73e88de11332df6
Moses Mumo <mosesm5499@gmail.com>
2023-09-20 04:47:00 +0300

    Update start.c

--- start.c
+++ start.c
@@ -5,7 +5,6 @@
 /**                                       
  * start - Handle the mode
  * Description: Mode can be INTERACTIVE or NON_INTERACTIVE                       
- *
  * @info: Struct of information about the shell                                       
  **/
 void start(general_t *info)
