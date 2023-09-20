62871b3a8554ed0e33e655a5162272589a78ce47
Moses Mumo <mosesm5499@gmail.com>
2023-09-20 04:35:00 +0300

    Update start.c

--- start.c
+++ start.c
@@ -2,13 +2,13 @@
 #include "main.h"
 #include "text.h"
 
-/**
+/**                                       
  * start - Handle the mode
- * Description: Mode can be INTERACTIVE or NON_INTERACTIVE
+ * Description: Mode can be INTERACTIVE or NON_INTERACTIVE                       
  *
- * @info: Struct of information about the shell
+ * @info: Struct of information about the shell                                       
  **/
 void start(general_t *info)
-{
+{                       
 	start_prompt(info);
 }
