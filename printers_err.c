735a9a7f995e6dcca6606b8ef486add0ea04a041
Moses Mumo <mosesm5499@gmail.com>
2023-09-20 04:33:14 +0300

    Update printers_err.c

--- printers_err.c
+++ printers_err.c
@@ -1,11 +1,11 @@
 #include "text.h"
-
-/**
+                             
+/**                   
  * print_err - Print a message to STDERR
- *
+ *                        
  * @msg: Message to print
  *
- * Return: Number of bytes printed
+ * Return: Number of bytes printed                                     
  **/
 int print_err(char *msg)
 {
