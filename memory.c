fab39e76d72b8230aa449b5432373f80fa276c50
Moses Mumo <mosesm5499@gmail.com>
2023-09-20 04:50:29 +0300

    Update memory.c

--- memory.c
+++ memory.c
@@ -2,11 +2,9 @@
 
 /**
  * _realloc - Reallocates a memory block using malloc and free
- *
  * @ptr: Pointer to the memory previously allocated
  * @old_size: Size, in bytes, of the allocated space for ptr
- * @new_size: New size, in bytes of the new memory block                                        
- *                                              
+ * @new_size: New size, in bytes of the new memory block                                                                                    
  * Return: Memory reallocated
  **/
 void *_realloc(void *ptr, size_t old_size, size_t new_size)                                            
